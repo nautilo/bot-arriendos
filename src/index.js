7c0ccdb5b6f0b72d1e8e6444ad460e28f09d65ca
nautilo <8lapse@gmail.com>
2022-12-23 12:11:16 -0300

    added bot client

--- src/index.js
+++ src/index.js
@@ -3,7 +3,35 @@ const server = express()
 const moment = require('moment')
 const qrcode = require('qrcode-terminal')
 const { Client, LocalAuth } = require('whatsapp-web.js')
-let now = moment().format()
+let now = moment().format("DD/MM/YYYY")
+/**
+ * Instancia del bot de Whatsapp
+ */
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
+
+
+client.on('qr', qr => {
+  qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+
+});
+
+client.on('message', message => {
+if(message.body === 'quediaes') {
+  message.reply(now);
+}
+});
+
+client.initialize();
+
+/**
+ * Render en html
+ */
 server.use((req, res) => {
   res.send(now)
 })
